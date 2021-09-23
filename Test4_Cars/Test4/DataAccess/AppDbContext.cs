88278dcdc1dcec3a51a2669a623061a457439b16
lorenzobasoc <87638986+lorenzobasoc@users.noreply.github.com>
2021-09-23 12:55:03 +0200

    test

---
+++ Test4_Cars/Test4/DataAccess/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Test4.Models;
+
+namespace Test4.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<RoadPoint> RoadPoints { get; set; }
+    }
+}
--- Test4_Cars/Test4/Program.cs
+++ Test4_Cars/Test4/Program.cs
@@ -1,11 +1,31 @@
 ﻿using System;
+using Test4.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
 
 namespace Test4
 {
     class Program
     {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseNpgsql("Host=localhost;Database=races;Username=lorenzobasoc;Password=ciao")
+                .Options;
+            var db = new AppDbContext(options);
+
+            var listCars = db.Cars
+                .Select(c => c)
+                .ToList();
+            var carThreads = listCars
+                .Select(c => new Thread(c.Run))
+                .ToList();
+            foreach (var t in carThreads) {
+                t.Start();
+            }
+
         }
+
+        
     }
 }
