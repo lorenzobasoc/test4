fc9c38a2ff7024412959c322da6e898571eebbbe
lorenzobasoc <87638986+lorenzobasoc@users.noreply.github.com>
2021-09-23 12:56:39 +0200

    test nuovo

--- Test4_Cars/Test4/Models/Car.cs
+++ Test4_Cars/Test4/Models/Car.cs
@@ -1,12 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using Test4.DataAccess;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
 
 namespace Test4.Models
 {
-    class Car
+    public class Car
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Cc { get; set; }
+        public int MaxSpeed { get; set; }
+
+        private DateTime _currentTime = DateTime.Now;
+        private int _totalPosition;       
+
+        public static CancellationTokenSource tokenSource;
+        public static event Action getLastRoadPoint;
+        public static event Action<Car> won;
+
+        public List<RoadPoint> RoadPoints { get; set; }
+
+        public async void Run() {
+            tokenSource = new CancellationTokenSource();
+            var db = GenerateDb();
+            while (true) {
+                if (tokenSource.IsCancellationRequested) {
+                    break;
+                }
+                var delay = GenerateRandom(1, 1000);
+                var casual = GenerateRandomDouble();
+                await Task.Delay(delay);
+                var newPosition = (int)casual * MaxSpeed;
+
+                RoadPoints.Add(new RoadPoint { Location = newPosition + _totalPosition, Time = _currentTime.AddMilliseconds(delay) });
+                getLastRoadPoint.Invoke();
+                db.SaveChanges();
+                if (_totalPosition >= 5000) {
+                    won.Invoke(this);
+                    break;
+                }
+            }    
+        }
+
+        public static int GenerateRandom(int startIndex, int stopIndex) {
+            return new Random().Next(startIndex, stopIndex + 1);
+        }
+
+        public static double GenerateRandomDouble() {
+            var random = new Random().NextDouble();
+            while (random < 0.5) {
+                random = new Random().NextDouble();
+            }
+            return random;
+        }
+
+        public static AppDbContext GenerateDb() {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseNpgsql("Host=localhost;Database=races;Username=lorenzobasoc;Password=ciao")
+                .Options;
+            return new AppDbContext(options);
+        } 
     }
 }
